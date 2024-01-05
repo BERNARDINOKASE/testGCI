7c6d2a688b00f691be15f4de99c6fd753425473b
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-01-05 23:09:30 +0800

    fixing migration

---
+++ database/migrations/2024_01_05_142600_create_transaksi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->required();
+            $table->unsignedBigInteger('product_id')->required();
+            // $table->string('voucher_id')->nullable();
+            $table->timestamps();
+
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreignUuid('voucher_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
